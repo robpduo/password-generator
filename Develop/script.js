8135c6a7ed67e25468415e1f5537947f6aea493b
robpduo <101683611+robpduo@users.noreply.github.com>
2022-05-25 09:50:44 -0400

    generates password of specified length

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,44 @@
 // Assignment code here
+const alphabetsLower = "abcdefghijklmnopqrstuvwxyz";
+const alphabetsHigher = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+let criteria = {
+  length: 0,
+}
+
+let randomize = () => {
+  let charSelector;
+  let charRestrict = 3;
+  let password = "password: ";
+
+  for (let i = 0; i < criteria.length; i ++) {
+    //generate random number between 1 and 3 to choose between lowercase, uppercase, and special characters
+    charSelector = Math.floor(Math.random() * charRestrict + 1);
+    console.log(charSelector);
+
+    if (charSelector == 1) { //append lowercase letter
+      password += alphabetsLower[Math.floor(Math.random() * 25)];
+
+    } else if (charSelector == 2) {  //append uppcase letter
+      password += alphabetsHigher[Math.floor(Math.random() * 25)];
+      
+    } else {
+      password += specialChar[Math.floor(Math.random() * specialChar.length)];
+
+    }
+
+
+  }
+
+  return password;
+}
+
+
 let generatePassword = () => {
-  
-  
+  criteria.length = window.prompt("Enter your desired length for the password(min: 8 characters, max 128 characters):");
+  return randomize();
+
 }
 
 // Get references to the #generate element
